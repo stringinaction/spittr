10da7762dff83d38535cabfe81d4ef88f31c75b6
scarecroweib <scarecroweib@gmail.com>
2016-08-16 17:32:43 +0800

    处理表单

--- src/main/java/com/weib/spittr/repostory/data/Spitter.java
+++ src/main/java/com/weib/spittr/repostory/data/Spitter.java
@@ -109,10 +109,7 @@ public class Spitter {
         if (!Objects.equals(this.username, other.username)) {
             return false;
         }
-        if (!Objects.equals(this.password, other.password)) {
-            return false;
-        }
-        return true;
+        return Objects.equals(this.password, other.password);
     }
     
     
